3037c492777ba8d1767e5f5d489a91dcbbe39e47
naveenkumarreddy1910 <naveen.k@mtechzilla.com>
2024-02-22 00:43:53 +0530

    fix: slider issues fixed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
+import {
+  Slider,
+  SliderFilledTrack,
+  SliderThumb,
+  SliderTrack,
+} from "@chakra-ui/react";
 
 const HomePage = () => {
-  return <div>index</div>;
+  return (
+    <div className="bg-slate-800 min-h-screen flex justify-center items-center">
+      <Fragment>
+        <div className="flex p-[1.25rem] w-[18.75rem] h-[18.75rem] bg-slate-500 shadow-md rounded-md">
+          <Slider aria-label="slider-ex-1" defaultValue={30}>
+            <SliderTrack>
+              <SliderFilledTrack />
+            </SliderTrack>
+            <SliderThumb />
+          </Slider>
+        </div>
+      </Fragment>
+    </div>
+  );
 };
 
 export default HomePage;
