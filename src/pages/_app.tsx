7bf7656f62831457480bec4f1b0dcfdd4a6d2d65
naveenkumarreddy1910 <naveen.k@mtechzilla.com>
2024-10-14 10:55:35 +0530

    fix: gtm tag add package style noqwe

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,7 @@ import "@/styles/globals.css";
 import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
 import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +11,22 @@ export default function App({ Component, pageProps }: AppProps) {
       <NavBar />
       <Component {...pageProps} />
       {/* <GoogleTagManager gtmId="GTM-MH47Z98G" /> */}
+
+      <Script
+        strategy="lazyOnload"
+        src={`https://www.googletagmanager.com/gtag/js?id=G-98BGRYZ38C`}
+      />
+
+      <Script id="" strategy="lazyOnload">
+        {`
+                  window.dataLayer = window.dataLayer || [];
+                  function gtag(){dataLayer.push(arguments);}
+                  gtag('js', new Date());
+                  gtag('config', 'G-98BGRYZ38C', {
+                    page_path: window.location.pathname,
+                    });
+                    `}
+      </Script>
     </ChakraProvider>
   );
 }
